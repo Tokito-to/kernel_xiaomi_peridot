3234c4d9dba0ccf0644742a21b80631b9c70b5b1
Navya Vemula <quic_nvemula@quicinc.com>
2025-03-26 10:38:32 +0530

    pinctrl: qcom: Add wakeup_enable and wakeup_present bit in Seraph
    Wakeup_enable bit sets the corresponding bit in the INTR_CFG register.
    Also correct target bit to reflect its true position within the
    INTR_CFG register. Add this support for Seraph SoC.
    
    Change-Id: Ie1a718cb4aab8a6db55fa003a31869a8c8b4c4c7
    Signed-off-by: Navya Vemula <quic_nvemula@quicinc.com>

--- drivers/pinctrl/qcom/pinctrl-seraph.c
+++ drivers/pinctrl/qcom/pinctrl-seraph.c
@@ -2,7 +2,6 @@
 /*
  * Copyright (c) 2024-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
-
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
@@ -40,7 +39,9 @@
 		.out_bit = 1,			\
 		.intr_enable_bit = 0,		\
 		.intr_status_bit = 0,		\
-		.intr_target_bit = 5,		\
+		.intr_target_bit = 8,		\
+		.intr_wakeup_enable_bit = 7,	\
+		.intr_wakeup_present_bit = 6,	\
 		.intr_target_kpss_val = 3,	\
 		.intr_raw_status_bit = 4,	\
 		.intr_polarity_bit = 1,		\
